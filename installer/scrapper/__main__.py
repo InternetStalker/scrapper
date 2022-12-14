a4ba5b01ce239f793d654825afdbe910bb289163
InternetStalker <internetstalcker@yandex.ru>
2022-12-14 09:24:49 +0300

    Add path to scrappers into __main__

--- installer/scrapper/__main__.py
+++ installer/scrapper/__main__.py
@@ -8,7 +8,9 @@ import pathlib
 
 def main():
     root_path = pathlib.Path(__file__).parent
-    asyncio.run(Package(root_path)())
+    path_to_scrappers = pathlib.Path(input("Enter path to folder where your scrapper will be: "))
+
+    asyncio.run(Package(root_path, path_to_scrappers)())
 
 if  __name__ == "__main__":
     main()
