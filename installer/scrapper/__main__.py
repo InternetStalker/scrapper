b68343d375af9303688fa53861dda3286d71e465
InternetStalker <internetstalcker@yandex.ru>
2023-02-16 10:59:35 +0300

    Move prepare_package, GithubUrl

--- installer/scrapper/__main__.py
+++ installer/scrapper/__main__.py
@@ -4,27 +4,17 @@ from __future__ import annotations
 import asyncio
 import pathlib
 
-import aiohttp
-
 from . import System
 from .prepare_package import Package
 
 
-async def prepare_package(path_to_scrappers: pathlib.Path):
-    "Manages all the logic of preparing"
-    package = Package(System().package_path, path_to_scrappers)
-    async with aiohttp.ClientSession() as session:
-        await asyncio.create_task(package.prepare_package(session))
-        await asyncio.create_task(package.prepare_webdrivers(session))
-        await asyncio.create_task(package.prepare_scrappers())
-
-        await package.create_logs()
-
-
 def main():
     path_to_scrappers = pathlib.Path(input(System().phrase_table["SCRAPPERS_PATH"]))
+    root_path = pathlib.Path(__file__).parent
 
-    asyncio.run(prepare_package(path_to_scrappers))
+    package = Package(root_path, path_to_scrappers)
+
+    asyncio.run(package.prepare_package(path_to_scrappers))
 
 if  __name__ == "__main__":
     main()
