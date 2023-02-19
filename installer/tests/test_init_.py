452253a68603320cee5a29db34fff4749745790d
InternetStalker <internetstalcker@yandex.ru>
2023-02-19 10:22:10 +0300

    Rename test_package_path

--- installer/tests/test_init_.py
+++ installer/tests/test_init_.py
@@ -1,13 +1,8 @@
-
-
 import pathlib
 
-from ..scrapper import System
+from .. import scrapper
 
 
-def test_root_path(tmp_path: pathlib.Path):
-        package_path = tmp_path / "package"
-        package_path.mkdir(exist_ok=True, parents=True)
-        scrapper_path = tmp_path / "sc"
-        scrapper_path.mkdir(exist_ok=True, parents=True)
-        assert System().package_path == package_path
+def test_package_path():
+        package_path = pathlib.Path(scrapper.__file__) / "package"
+        assert scrapper.System().package_path == package_path
